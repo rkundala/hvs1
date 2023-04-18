93fd7c394a71aafe08b11a0831f6804f07498e58
rkundala <rkundala@gmail.com>
2023-04-18 10:44:39 -0400

    a

--- cypress/e2e/home-values/SecureLogin.cy.js
+++ cypress/e2e/home-values/SecureLogin.cy.js
@@ -1,12 +1,12 @@
 
 describe('Example to demonstrate handling of Basic Auth in Cypress', () => {
 
-  it('Successfully login by appending username and password in URL', function () {
+  it('Successfully login by appending username and password in url', function () {
       cy.visit('https://admin:admin@the-internet.herokuapp.com/basic_auth')
       cy.get('p').should('include.text', 'Congratulations! You must have the proper credentials.')
   })
 
-  it('Successfully login using headers', function () {
+  it('Successfully login using headers', function  () {
       cy.visit("https://the-internet.herokuapp.com/basic_auth", {
           headers: {
               authorization: 'Basic YWRtaW46YWRtaW4='
